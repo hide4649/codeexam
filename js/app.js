14d6a0aacc558c12d1e74688cf48ae0fe1217654
hide4649 <tokuchendesu02@yahoo.co.jp>
2020-05-26 22:02:51 +0900

    今日は以下しました
    1.レスポンシブ再調整
    ２デザイン修正

--- js/app.js
+++ js/app.js
@@ -6,21 +6,8 @@ function getyorpositon(){
     var lng = pos.coords.longitude;
     document.getElementById("latitude").value = lat;
     document.getElementById("longitude").value = lng;
-    var req = new XMLHttpRequest();
-    var appid = "AIzaSyAvz1BK-7cFFPBOZEW4Q1gfceW1AF78jAA"
-    var url = `https://maps.googleapis.com/maps/api/geocode/json?appid=${appid}&latitude=${lat}&longitude=${lng}`;
-  
-
-    req.responseType = 'json';
-    
-    req.open('GET', url, true);
-
-    req.onload = function(){
-      document.getElementById('address').value = req.response.pref_name;
-    }
-
-    req.send();
-  })
+    document.getElementById("result").value = "現在地が取得できました";
+  }) 
 }
 
 function gnaviFreewordSearch(offset, hit_per_page){
@@ -48,13 +35,17 @@ function gnaviFreewordSearch(offset, hit_per_page){
       = `<div class="card text-white bg-dark mb-5">
             <div class="row justify-content-center">
               <div class="col-5 pr-0">
-                <img class="card-img w-100 h-100" src="${req.response.rest[i].image_url.shop_image1}" alt="no image">
+                <img class="card-img w-100 h-100" src="${req.response.rest[i].image_url.shop_image1}" onerror="this.src='image/noimage.jpg'; this.removeAttribute('onerror'); this.removeAttribute('onload');"
+                onload="this.removeAttribute('onerror'); this.removeAttribute('onload');" alt="no image">
               </div>
               <div class="col-7 card-body-padding">
                 <div class="card-body">
-                  <h5 class="card-title">${req.response.rest[i].name}<br>${req.response.rest[i].access.line} ${req.response.rest[i].access.station}</h5>
+                  <h5 class="card-title">${req.response.rest[i].name}</h5>
+                  <h6><最寄り駅>${req.response.rest[i].access.line} ${req.response.rest[i].access.station}</h5>
+                  <div class="card-content border-success border-top border-bottom">
                   <p class="card-text">${req.response.rest[i].pr.pr_short }</p>
-                  <button type="button" class="btn btn-primary " data-toggle="modal" data-target="#exampleModal_${[i]}">
+                  </div>
+                  <button type="button" class="btn btn-primary mt-2" data-toggle="modal" data-target="#exampleModal_${[i]}">
                     店舗情報はこちら
                   </button>
                 </div>
@@ -65,26 +56,26 @@ function gnaviFreewordSearch(offset, hit_per_page){
           <div class="modal fade " id="exampleModal_${[i]}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog " role="document">
               <div class="modal-content bg-dark text-white">
-                <div class="modal-header">
+                <div class="modal-header border-success border-bottom">
                   <h5 class="modal-title" id="exampleModalLabel">${req.response.rest[i].name}</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body border-success border-bottom">
                 <img class="mb-2 w-100 h-100" src = ${req.response.rest[i].image_url.shop_image1}>
                 <p><店舗紹介></p>
-                <p>${req.response.rest[i].pr.pr_long }</p>
+                <p class="border-success border-top border-bottom">${req.response.rest[i].pr.pr_long }</p>
                 <p><住所></p>
-                <p>${req.response.rest[i].address}</p>
+                <p class="border-success border-bottom">${req.response.rest[i].address}</p>
                 <p><アクセス></p>
-                <p>${req.response.rest[i].access.line} ${req.response.rest[i].access.station} ${req.response.rest[i].access.walk}分</p>
+                <p class="border-success border-bottom">${req.response.rest[i].access.line} ${req.response.rest[i].access.station} ${req.response.rest[i].access.walk}分</p>
                 <p><電話番号></p>
-                <p>${req.response.rest[i].tel}</p>
+                <p class="border-success border-bottom">${req.response.rest[i].tel}</p>
                 <p><営業時間></p>
-                <p>${req.response.rest[i].opentime} 　⚠休業日 ${req.response.rest[i].holiday}</p>
+                <p class="border-success border-bottom">${req.response.rest[i].opentime} 　⚠休業日 ${req.response.rest[i].holiday}</p>
                 <p><予算></p>
-                <p>平均予算:${req.response.rest[i].budget}  ランチ平均予算:${req.response.rest[i].lunch}</p>
+                <p class="border-success border-bottom">平均予算:${req.response.rest[i].budget}  ランチ平均予算:${req.response.rest[i].lunch}</p>
                 <p><カード・電子マネー></p>
                 <p>カード:${req.response.rest[i].credit_card}</p>
                 <p>電子マネー:${req.response.rest[i].e_money}</p>
@@ -97,8 +88,14 @@ function gnaviFreewordSearch(offset, hit_per_page){
               </div>
             </div>
           </div>`;
+
           if(i > 9){
-            document.getElementById('next').innerHTML ='<a href = "#" onclick="gnaviFreewordSearch(offset+=hit_per_page, hit_per_page);">次の結果</a><a href = "#" onclick="gnaviFreewordSearch(offset-=hit_per_page, hit_per_page);">前の結果</a>';
+            document.getElementById('next').innerHTML =`<nav aria-label="Page navigation example">
+            <ul class="pagination">;
+              <li class="page-item"><a class="page-link" href="#" onclick="gnaviFreewordSearch(offset-=hit_per_page, hit_per_page);">前へ</a></li>
+              <li class="page-item"><a class="page-link" href="#" onclick="gnaviFreewordSearch(offset+=hit_per_page, hit_per_page);">次へ</a></li>
+            </ul>
+          </nav>`;
           }
         }
     };
